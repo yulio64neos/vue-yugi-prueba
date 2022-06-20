0e760f6085305f881b234ca4ff28077b3e7361c4
yulio64neos <julio64neos@gmail.com>
2022-06-19 23:03:54 -0500

    Se realizo de forma exitosa la consulta de las cartas de YUGI

---
+++ src/components/Consumir.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div v-for="item in result" :key="item.id" class="content">          
+          <img v-bind:src="item.card_images[0].image_url_small">
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data: () =>({
+        result: []
+    }),
+    created(){
+        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+        .then((result) =>{
+            this.result = result.data.data
+            console.log(result.data.data);
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Yugi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Yugioh</h1>
+      <Consumir/>
+  </div>
+</template>
+
+<script>
+import Consumir from '../components/Consumir.vue'
+export default {
+    components:{
+        Consumir
+    }
+}
+</script>
+
+<style>
+
+</style>
